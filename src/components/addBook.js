388a154d8efceeca3225baffbbdab54aa0de825c
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-06-02 16:17:45 -0300

    add book

---
+++ src/components/addBook.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AddBook = () => (
+  <div>
+    <h2>ADD NEW BOOK</h2>
+    <form>
+      <input placeholder="Book title" />
+      <input placeholder="Author" />
+      <button type="submit">ADD BOOK</button>
+    </form>
+  </div>
+);
+
+export default AddBook;
--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Book from '../components/book';
 import './books.css';
+import AddBook from '../components/addBook';
 
 const Books = () => (
   <div className="books">
@@ -9,6 +10,9 @@ const Books = () => (
       <li className="book"><Book genre="Science Fiction" title="Dune" author="Frank Herbert" /></li>
       <li className="book"><Book title="Capital in the Twenty-First Century" author="Suzanne Collins" /></li>
     </ul>
+    <div>
+      <AddBook />
+    </div>
   </div>
 );
 
