c9b42bd6e51ccb5cd0a12458c2fa4e2a3993052c
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-06-06 18:33:16 -0300

    conect APP with Redux store

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,23 @@
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const DELETE_BOOK = 'bookstore/books/DELETE_BOOK';
 
-const booksArray = [];
+const booksArray = [
+  {
+    id: 1,
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+  },
+  {
+    id: 2,
+    title: 'Dune',
+    author: 'Frank Herbert',
+  },
+  {
+    id: 3,
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+  },
+];
 
 const booksReducer = (state = booksArray, action) => {
   switch (action.type) {
