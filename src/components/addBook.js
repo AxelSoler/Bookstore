991646146ac37c007920e90fbd75b79f3f8b04bf
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-06-07 12:39:31 -0300

    add book with different id

--- src/components/addBook.js
+++ src/components/addBook.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 import { addNewBook } from '../redux/books/books';
 
 const AddBook = () => {
@@ -7,16 +8,17 @@ const AddBook = () => {
 
   const submitBook = (e) => {
     e.preventDefault();
-    const id = 4;
+    const id = uuidv4();
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     dispatch(addNewBook({ id, title, author }));
+    document.getElementById('form').reset();
   };
 
   return (
     <div>
       <h2>ADD NEW BOOK</h2>
-      <form onSubmit={submitBook}>
+      <form id="form" onSubmit={submitBook}>
         <input id="title" placeholder="Book title" />
         <input id="author" placeholder="Author" />
         <button type="submit">ADD BOOK</button>
