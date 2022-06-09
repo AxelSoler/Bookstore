d98efe498c7271931755b6ba4bf6d63c7b23a45b
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-06-09 17:19:22 -0300

    created circle progression

--- src/components/book.js
+++ src/components/book.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { CircularProgressBar } from '@tomik23/react-circular-progress-bar';
 import { deleteBookAPI } from '../redux/books/books';
 import './book.css';
 
@@ -12,18 +13,40 @@ const Book = (props) => {
 
   return (
     <div className="bookSection">
-      <div className="bookName">
-        <h3>
-          {title}
-        </h3>
-        <h4>
-          {author}
-        </h4>
+      <div className="leftPart">
+        <div className="bookName">
+          <h3>
+            {title}
+          </h3>
+          <h4>
+            {author}
+          </h4>
+        </div>
+        <div className="buttonSection">
+          <button type="button">Comments</button>
+          <button onClick={removeBook} type="button">Remove</button>
+          <button id="editBtn" type="button">Edit</button>
+        </div>
       </div>
-      <div className="buttonSection">
-        <button type="button">Comments</button>
-        <button onClick={removeBook} type="button">Remove</button>
-        <button id="editBtn" type="button">Edit</button>
+      <div className="completed">
+        <CircularProgressBar
+          percent="15"
+          animationSmooth="ls ease-out"
+          colorCircle="#f1f1f1"
+          colorSlice="#0290ff"
+          size={100}
+        />
+        <div className="completedBook">
+          <p>15%</p>
+          <p>Completed</p>
+        </div>
+      </div>
+      <div className="bookChapter">
+        <p>Current Chapter</p>
+        <p>Chapter 4</p>
+        <button type="button">
+          Update progress
+        </button>
       </div>
     </div>
   );
